d8dc7c5f51718d1e660d73c8aa145b654e814d32
SkyeN <skye@dames.es>
2018-08-16 14:01:57 -0700

    check for perfect square and convert everything to lower case

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
 //BACK-END
+
 var removeChar = function (input) {
     var newInput = "";
     for (i = 0; i < input.length; i++) {
         if (input[i].match(/^[a-zA-Z0-9]$/)) {
-            newInput += input[i];
+            newInput += input[i].toLowerCase();
             // console.log(newInput[i]);
         }
     }
     return newInput;
 }
 
-// var encoder = function (input) {
+var encoder = function (newInput) {
 
-// }
+    var row = Math.ceil(Math.sqrt(newInput.length));
+
+    if (Math.pow(row,2) > newInput.length) {
+        var column = row -1;
+    }
+    else {
+        var column = row;
+    }
+    
+    return [row,column];
+}
 
 //FRONT-END
 $(document).ready(function () {
     $("form#input").submit(function (event) {
         event.preventDefault();
         var inputStr = $("#inputStr").val();
-        // console.log(removeChar(inputStr));
+        console.log(encoder(removeChar(inputStr)));
+        $("encoderStr").empty();
         $("#encodedStr").append(removeChar(inputStr));
     })
 })
